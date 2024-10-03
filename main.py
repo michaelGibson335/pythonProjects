6d8f71d287550dd478d0f0d189017ec5575f1934
michaelGibson335 <berserker_lion@yahoo.com>
2024-10-02 21:25:26 -0400

    Todo project

--- main.py
+++ main.py
@@ -3,18 +3,21 @@
 todos = []
 
 while True:
-    user_action = input("Type add or show, or exit: ")
+    user_action = input("Type add, show, edit or exit: ")
     user_action = user_action.strip()
 
     match user_action:
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
-        case 'show':
+        case 'show' | 'display':
             for item in todos:
+                item = item.title()
                 print(item)
+        case 'edit':
+            number = input("Number of the todo to edit")
         case 'exit':
             break
-
+        
 print("Bye!")
     
