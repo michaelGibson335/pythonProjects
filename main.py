7cdf996561a2f02933733f159071474e346a1d0c
michaelGibson335 <berserker_lion@yahoo.com>
2024-10-03 21:39:42 -0400

    Update to ToDo

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
-
-
+##To Do project
+##this program is a to do list that prompts a user to update a list using match and case actions
+#to add, show, edit, complete or edit an item
 todos = []
 
+#This is the main loop that runs the program continuously
+#while true, unless they select exit and the program breaks
 while True:
-    user_action = input("Type add, show, edit or exit: ")
+    #prompt user to input add, show, edit, complete or exit and store in user_action
+    user_action = input("Type add, show, edit, complete or exit: ")
+    #takes the user action and strips any whitespace
     user_action = user_action.strip()
-
+    
+    #use match case structure that matches add, show, edit, complete or exit, depending on
+    #the user input
     match user_action:
+        #case add, this allows the user to update the todos list with their input
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
+        #case show, displays the items in the array so far
+        #uses for loop and enumerate to display items in a numberical fashion
+        #with a dash in between
         case 'show' | 'display':
-            for item in todos:
-                item = item.title()
-                print(item)
+            for index, item in enumerate(todos):
+                row = f"{index + 1}-{item}"
+                print(row)
+        #case edit, allow user to update todos list by selecting
+        #index of todo item and replace it with a new input        
         case 'edit':
-            number = input("Number of the todo to edit")
+            number = int(input("Number of the todo to edit: "))
+            number -= 1
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
+        #complete, this allows a user to remove the last item off the list    
+        case 'complete':
+            number = int(input("Number of the todo to complete: "))
+            todos.pop(number)
+        #case exit, breaks out of the loop once exit is input    
         case 'exit':
             break
         
