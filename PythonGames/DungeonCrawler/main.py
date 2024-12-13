66be0edec44ca0d047f7747e245cd7064c4d5fb8
michaelGibson335 <berserker_lion@yahoo.com>
2024-12-13 15:51:19 -0500

    main.py coding

---
+++ PythonGames/DungeonCrawler/main.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.init()
+
+#display game screen
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+#set game window screen, pass Screen_Width and height variable to it
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
