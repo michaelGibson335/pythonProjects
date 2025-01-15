46af06314b0626b0fc1546e9340c634f05d3693c
michaelGibson335 <berserker_lion@yahoo.com>
2025-01-15 11:32:48 -0500

    Character bow creation and rendering

--- PythonGames/DungeonCrawler/constants.py
+++ PythonGames/DungeonCrawler/constants.py
@@ -1,17 +1,29 @@
-#set frames per rate for game
 FPS = 60
-
-#display game screen
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-
-#screen and item scaling
 SCALE = 3
-
-#set variable for speed
+BUTTON_SCALE = 1
+WEAPON_SCALE = 1.5
+ITEM_SCALE = 3
+POTION_SCALE = 2
+FIREBALL_SCALE = 1
 SPEED = 5
+ARROW_SPEED = 10
+FIREBALL_SPEED = 4
+ENEMY_SPEED = 4
+OFFSET = 12
+TILE_SIZE = 16 * SCALE
+TILE_TYPES = 18
+ROWS = 150
+COLS = 150
+SCROLL_THRESH = 200
+RANGE = 50
+ATTACK_RANGE = 60
 
-#part of the character display coloring
-#and coloring for background
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PINK = (235, 65, 54)
 RED = (255, 0, 0)
 BG = (40, 25, 25)
+MENU_BG = (130, 0, 0)
+PANEL = (50, 50, 50)
